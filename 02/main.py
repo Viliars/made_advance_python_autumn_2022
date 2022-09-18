89b70c91b6a302acfe71e2922c25b20a0152abd6
Михаил Притугин <mpritugin@MacBook-Air-Mihail.local>
2022-09-18 20:42:53 +0300

    fix HW2

--- 02/main.py
+++ 02/main.py
@@ -3,7 +3,20 @@ def near_zero(l: list) -> list:
     return [l[i] for i in range(len(l)) if abs(l[i]) == minimum]
 
 def merge(l1: list, l2: list) -> list:
-    return list(set(l1) & set(l2))
+    used = set()
+    l1_index, l2_index = 0, 0
+    result = []
+
+    while l1_index < len(l1) and l2_index < len(l2):
+        if l1[l1_index] == l2[l2_index] and l1[l1_index] not in used:
+            result.append(l1[l1_index])
+            used.add(l1[l1_index])
+        elif l1[l1_index] < l2[l2_index]:
+            l1_index += 1
+        else:
+            l2_index += 1
+
+    return result
 
 if __name__ == "__main__":
     assert near_zero([-5, 9, 6, -8]) == [-5]
