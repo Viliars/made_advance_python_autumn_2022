60593cee29e5e2cff8223b8b4928fec0b11b0ad6
Михаил Притугин <mpritugin@MacBook-Air-Mihail.local>
2022-09-25 22:39:55 +0300

    Added asserts

--- 03/main.py
+++ 03/main.py
@@ -3,6 +3,7 @@ import functools
 import time
 import numpy as np
 import math
+from collections import Counter
 
 # ----- task №1 -----
 def parse_json(json_str: str, required_fields=None, keywords=None, keyword_callback=None) -> None:
@@ -62,16 +63,27 @@ def mean(k):
         return _wrapper
     return mean_decorator
 
-@mean(3)
+k = 3
+
+@mean(k)
 def foo(sec):
     time.sleep(sec)
 
+stats = Counter()
+
+def for_test(word: str) -> None:
+    global stats
+    stats[word] += 1
 
 if __name__ == "__main__":
     # task №1
-    parse_json('{"key1": "Word1 word2", "key2": "word2 word3"}', ["key1"], ["word2"], print)
+    parse_json('{"key1": "Word1 word2", "key2": "word2 word3"}', ["key1"], ["word2"], for_test)
+    assert stats['word2'] == 1
+    parse_json('{"key1": "Word1 word2", "key2": "word2 word3"}', ["key1"], ["Word1", "word2"], for_test)
+    assert stats['Word1'] == 1
+    assert stats['word2'] == 2
 
-    # task №2
+    # тесты MaxList
     l = MaxList(3)
     assert l == []
     l.append(1)
@@ -86,5 +98,11 @@ if __name__ == "__main__":
     assert l == [3, 4, 5]
     assert math.isclose(l.get_mean(), 4.0)
 
+    # task №2
+    sum = 0
     for i in range(1, 10):
+        sum += i
         foo(i)
+        print(foo.__logs.get_mean(), sum / i)
+        if i <= k:
+            assert abs(foo.__logs.get_mean() - sum / i) < 0.1
